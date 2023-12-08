c4f12a399baf6de8ef3090736879ebbff090671a
yeonkr <1714882@sookmyung.ac.kr>
2023-12-08 16:33:01 +0900

    refactor: Change API entry point to AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NoteModule } from './note/note.module';
+import { AppModule } from './app.module';
 
 const PORT = 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(NoteModule);
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Diary API')
     .setDescription('The Diary API Documentation')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
