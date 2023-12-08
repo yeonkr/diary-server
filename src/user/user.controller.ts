0650831e69cfc2c4ca6a39ee943cdcd0ac2419b6
yeonkr <1714882@sookmyung.ac.kr>
2023-12-08 16:29:09 +0900

    feat: add auth api tags

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/signup')
-  async signUp(@Body() data: User): Promise<User> {
+  async createUser(@Body() data: User): Promise<User> {
     return this.userService.createUser(data);
   }
 }
