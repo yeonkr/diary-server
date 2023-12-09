8ad4e26676eb78cee26278ea47c1a03284667be9
yeonkr <1714882@sookmyung.ac.kr>
2023-12-10 00:16:55 +0900

    feat: jwt 모듈 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy],
 })
 export class UserModule {}
