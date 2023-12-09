01ef625a082d5c6c8c03bf0175502cfda2ac67eb
yeonkr <1714882@sookmyung.ac.kr>
2023-12-10 02:11:39 +0900

    feat: 로그인 및 jwt 토큰 발급 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/user.dto';
+import { CreateUserDto, LoginUserDto } from './dto/user.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -11,5 +11,10 @@ export class UserController {
   @Post('/signup')
   async createUser(@Body() data: CreateUserDto) {
     return this.userService.createUser(data);
+  }
+
+  @Post('/login')
+  async loginUser(@Body() data: LoginUserDto) {
+    return this.userService.loginUser(data);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateUserDto } from './dto/user.dto';
+import { CreateUserDto, LoginUserDto } from './dto/user.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async createUser(data: CreateUserDto) {
     return this.prisma.user.create({
       data: data,
     });
+  }
+
+  async loginUser(data: LoginUserDto) {
+    const user = await this.findByEmail(data.email);
+
+    if (!user) {
+      throw new UnauthorizedException('이메일이 존재하지 않습니다.');
+    }
+
+    const isSamePassword = bcrypt.compareSync(data.password, user.password);
+
+    if (!isSamePassword) {
+      throw new ForbiddenException('비밀번호를 확인해주세요.');
+    }
+
+    const token = await this.getToken(user.id, user.email, user.name);
+
+    return {
+      message: '로그인 성공',
+      token,
+    };
+  }
+
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async getToken(id: number, email: string, name: string) {
+    const payload = { id, email, name };
+    return this.jwtService.sign(payload);
   }
 }
