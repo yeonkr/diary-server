b665caed26003d3907c5638fe4237540c21d812f
yeonkr <1714882@sookmyung.ac.kr>
2023-12-11 19:14:54 +0900

    feat: Added Swagger ApiProperty to CreateNoteDto

--- src/note/dto/note.dto.ts
+++ src/note/dto/note.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateNoteDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   content: string;
 }
