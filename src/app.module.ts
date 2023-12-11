1755b557ae5be41ea2e386abc8b7c00d48d49b7c
yeonkr <1714882@sookmyung.ac.kr>
2023-12-11 19:08:48 +0900

    feat: Updated AppModule and Added ConfigsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { NoteModule } from './note/note.module';
+import { JwtMiddleware } from './user/jwt/jwt.middleware';
+import { ConfigsModule } from './configs/configs.module';
 
 @Module({
-  imports: [UserModule, NoteModule],
+  imports: [ConfigsModule, UserModule, NoteModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware).forRoutes('*');
+  }
+}
