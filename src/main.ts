ecc96911fdca5722b2b9b93e6a43028c4d8c9621
yeonkr <1714882@sookmyung.ac.kr>
2023-12-14 21:09:36 +0900

    feat: Swagger 설정 및 API 문서 생성 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { writeFileSync } from 'fs';
+import * as path from 'path';
 
 const PORT = 4000;
 
@@ -11,13 +13,30 @@ async function bootstrap() {
     .setTitle('Diary API')
     .setDescription('The Diary API Documentation')
     .setVersion('1.0')
-    .addBearerAuth()
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+  });
+
+  const outputPath = path.join(__dirname, '../generate/openapi.json');
+  writeFileSync(outputPath, JSON.stringify(document));
+
   SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: `api/json`,
+    yamlDocumentUrl: `api/yaml`,
+    explorer: true,
+    customSiteTitle: 'api',
     swaggerOptions: {
-      port: PORT,
+      docExpansion: 'none',
+      persistAuthorization: true,
+      // displayOperationId: true,
+      operationsSorter: 'method',
+      tagsSorter: 'alpha',
+      tryItOutEnabled: true,
+      filter: true,
+      deepLinking: true,
     },
   });
 
